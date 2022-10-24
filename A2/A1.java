7c3deb8d7e026b5206efcb52ca94aad8d9f71e83
Matthew Walters <mattwalters@MacBook-Pro.local>
2022-10-24 17:29:39 -0400

    added files for assignment 2

---
+++ A2/A1.java
@@ -0,0 +1,13 @@
+import util.MyWekaUtils;
+
+/*
+ * The Main function for Assignment 2; Uses methods in MyWekaUtils
+ * Note: Static methods in MyWekaUtils can be used without instantiating
+ */
+public class A1 {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+
+    }
+  }
+  
---
+++ A2/README.md
@@ -0,0 +1,9 @@
+#### Assignment 1 (Weka Classification via Code)
+
+How to compile:
+
+    javac A1.java util/MyWekaUtils.java -classpath weka.jar -Xlint
+
+How to execute:
+
+    java A1
