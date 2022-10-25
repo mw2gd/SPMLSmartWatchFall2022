6811a713a574736068832f0a9bb269f382e3fe5b
vbhaip <vbhaip01@gmail.com>
2022-10-25 13:04:18 -0400

    add other features

--- A2/features.py
+++ A2/features.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import os
-import statistics # statistics.mean and statistics.stdev
+import statistics # statistics.mean, statistics.stdev, statistics.median
 
 TIME_SLICE = 1
 
+cols = ['mean_x', 'std_x', 'median_x', 'rms_x', 'mean_y', 'std_y', 'median_y', 'rms_y','mean_z', 'std_z', 'median_z', 'rms_z','Activity'] 
+
+def rms(vals):
+    sq_vals = [val**2 for val in vals]
+    return statistics.mean(sq_vals)**0.5
+
+
 def features(entries):
-    df_final = pd.DataFrame(columns=['mean_x', 'std_x', 'mean_y', 'std_y', 'mean_z', 'std_z', 'Activity'])
+    df_final = pd.DataFrame(columns=cols)
 
     for entry in entries:
         if (".DS_Store" in entry): continue
@@ -23,10 +30,10 @@ def features(entries):
         window_size = int(num_entries/num_seconds)
         print("window=" + str(window_size))
 
-        df_features = pd.DataFrame(columns=['mean_x', 'std_x', 'mean_y', 'std_y', 'mean_z', 'std_z', 'Activity'])
+        df_features = pd.DataFrame(columns=cols)
 
         # calculate features for each second's worth of data
-        for i in range (0, num_seconds-TIME_SLICE):
+        for i in range (0, num_seconds-TIME_SLICE + 1):
             start = i*window_size
             end   = start+window_size*TIME_SLICE
             x_accel = df.iloc[start:end,1]
@@ -37,10 +44,16 @@ def features(entries):
             {
                 "mean_x": [statistics.mean(x_accel)],
                 "std_x":  [statistics.stdev(x_accel)],
+                "median_x":  [statistics.median(x_accel)],
+                "rms_x":  [rms(x_accel)],
                 "mean_y": [statistics.mean(y_accel)],
                 "std_y":  [statistics.stdev(y_accel)],
+                "median_y":  [statistics.median(y_accel)],
+                "rms_y":  [rms(y_accel)],
                 "mean_z": [statistics.mean(z_accel)],
                 "std_z":  [statistics.stdev(z_accel)],
+                "median_z":  [statistics.median(z_accel)],
+                "rms_z":  [rms(z_accel)],
                 "Activity": activity
             }
             )
@@ -53,8 +66,8 @@ def features(entries):
         df_final = pd.concat([df_final, df_features])
         break
 
-    # print(df_final)
-    # df_final.to_csv('./features.csv', encoding='utf-8', index=False, header=True)
+     # print(df_final)
+     # df_final.to_csv('./features.csv', encoding='utf-8', index=False, header=True)
 
 
 def main():
