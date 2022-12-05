aa1b93019605072762f7854a375b56eb91b0a722
Matthew Walters <mw2gd@virginia.edu>
2022-12-05 13:34:27 -0500

    completed python file that automates classification

---
+++ FinalProject/A2.java
@@ -0,0 +1,66 @@
+import util.MyWekaUtils;
+
+/*
+ * The Main function for Assignment 2; Uses methods in MyWekaUtils
+ * Note: Static methods in MyWekaUtils can be used without instantiating
+ */
+public class A2 {
+    public static void main(String[] args) {
+        int[] featureIndices;
+        Integer model = 0;
+        String filePath = "";
+        String[][] csvData;
+        String arffData;
+
+        /*
+         * Check for command line arguments (filepath, featureindices)
+         */
+        if (args.length < 3) {
+            System.out.println("Missing Arguments:\n\t<path to csv>\n\t<model type> (1=Decision Tree, 2=Random Forest, 3=SVM)\n\t<features indices> (column number of feature(s) in CSV)\n\tExample (uses columns 0,1,4,5,8,9): java -cp .:util/weka.jar A2 features.part1.csv 1 0 1 4 5 8 9");
+            return;
+        }
+
+        /*
+        * My idea is to execute the python script before running this
+        * CSV file with features must already exist
+        */
+        filePath = args[0]; // Add path to preprocessed csv 
+        model = Integer.parseInt(args[1]);
+
+        featureIndices = new int[args.length - 2];
+        // pass unknown number of feature indices to command line
+        for (int i = 2; i < args.length; i++) {
+            featureIndices[i-2] = Integer.parseInt(args[i]); 
+        }
+        
+        // System.out.println(filePath);
+        // System.out.println(model.toString());
+        // System.out.println(Arrays.toString(featureIndices));
+
+        try {
+            csvData  = MyWekaUtils.readCSV(filePath);
+            arffData = MyWekaUtils.csvToArff(csvData, featureIndices);
+        }
+        catch(Exception e) {
+            System.out.println(e); // print excpetion that was thrown
+            return;
+        }
+
+        // Sanity check
+        //for(String[] x: csvData)  System.out.println(Arrays.toString(x));
+        //System.out.println(arffData);
+
+        /*
+         * Build Classifier and get accuracy %
+         */       
+        try {
+            double eval = MyWekaUtils.classify(arffData, model);
+            System.out.println("\nACCURACY: " + Double.toString(eval) + "%");
+        }
+        catch(Exception e) {
+            System.out.println(e); // print excpetion that was thrown
+            return;
+        }
+    }
+  }
+  
---
+++ FinalProject/classify.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import subprocess
+from sklearn.model_selection import train_test_split
+
+# Files containing the training data
+csv_train_files = ["features-timeslice-1.csv", "features-timeslice-2.csv", "features-timeslice-3.csv", 
+             "features-timeslice-4.csv", "features-timeslice-5.csv"]
+
+# number of features in the training data
+num_features = 15
+
+def split(data):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1) # 0.25 x 0.8 = 0.2
+    return X_train, y_train, 
+
+
+def parse_csv():
+    return None
+
+def classify(file, model):
+
+    columns = list(range(0,15))
+    accuracy_improves = True
+    max_accuracy     = -1
+    overall_accuracy = -1
+    feature_set = []
+
+    while (accuracy_improves):
+
+        for i in columns:
+            # Run A2.java to train models in WEKA
+            # args: <path to csv> <model type (1=Decision Tree, 2=Random Forest, 3=SVM)> <features indices>
+            result = subprocess.check_output(["java", "-cp", ".:util/weka.jar", "A2", file, model] + feature_set + [str(i)], stderr=subprocess.STDOUT)
+            #print("java -cp .:util/weka.jar A2 " + file + " " + model + " " + str(feature_set + [i]))
+            result = result.decode('utf-8').split(' ')
+
+            # Obtain accuracy of the model for that feature combo
+            accuracy = float(result[-1].replace('%', ''))
+            #print(feature_set + [str(i)], " ", accuracy)
+            
+            if (accuracy > max_accuracy):
+                best_feature = i
+                max_accuracy = accuracy
+        
+
+        #print("best_feature = " + str(best_feature) + " with acc=" + str(max_accuracy))
+
+        if (not(str(best_feature) in feature_set)):
+            feature_set.append(str(best_feature))
+            columns.remove(best_feature)
+
+        if (max_accuracy > overall_accuracy):
+            overall_accuracy = max_accuracy
+
+            if (len(columns) < 1):
+                accuracy_improves = False
+        else:
+            accuracy_improves = False
+
+    print(feature_set, " final accuracy=", str(overall_accuracy))
+
+    result = subprocess.check_output(["java", "-cp", ".:util/weka.jar", "A2", file, model] + [str(i) for i in range(0,15)], stderr=subprocess.STDOUT)
+    result = result.decode('utf-8').split(' ')
+
+    # Obtain accuracy of the model for that feature combo
+    accuracy = float(result[-1].replace('%', ''))
+    print([str(i) for i in range(0,15)], " ", accuracy) 
+
+def main():
+    # Compile A2.java
+    subprocess.run(["javac", "-classpath", "util/weka.jar", "util/MyWekaUtils.java", "A2.java"])
+
+    for file in csv_train_files:
+        print("FILE: " + "'" + file + "'")
+        print("Decision Tree")
+        classify(file, "1")
+        print("Random Forest")
+        classify(file, "2")
+        print("")
+
+if __name__=="__main__":
+    main()
